3ddfa598cf7f0e30ce9d479a45e42c27767c19b2
ElliotLuque <elliotluque@gmail.com>
2025-04-30 11:47:51 +0200

    feat: should not cancel reservation test

--- test/src/ReservationSystem.Test/Reservations/Application/Cancel/CancelReservationUseCaseTest.cs
+++ test/src/ReservationSystem.Test/Reservations/Application/Cancel/CancelReservationUseCaseTest.cs
@@ -46,6 +46,41 @@ public class CancelReservationUseCaseTest
   }
   
   [Fact]
+  public async Task ShouldNotCancelReservation()
+  {
+    // Given
+    var reservationId = new ReservationId(Guid.NewGuid());
+    var reservation = new Reservation(
+      reservationId,
+      new UserId(Guid.NewGuid()),
+      new SpaceId(Guid.NewGuid()),
+      new ReservationDateRange(DateTime.UtcNow.AddHours(1), DateTime.UtcNow.AddHours(2)),
+      ReservationStatus.Canceled
+    );
+    
+    var reservationRepoMock = new Mock<IReservationRepository>();
+    reservationRepoMock
+      .Setup(repo => repo.FindByIdAsync(It.IsAny<ReservationId>()))
+      .ReturnsAsync(reservation);
+
+    var useCase = new CancelReservationUseCase(
+      reservationRepoMock.Object
+    );
+
+    var command = new CancelReservationCommand(
+      ReservationId: reservationId.Value
+    );
+    
+    // When & Then
+    var exception = await Assert.ThrowsAsync<InvalidOperationException>(() => useCase.Execute(command));
+    Assert.Equal("Reservation already canceled", exception.Message);
+    reservationRepoMock.Verify(
+      x => x.SaveAsync(reservation),
+      Times.Never
+    );
+  }
+  
+  [Fact]
   public async Task ShouldErrorWhenReservationNotFound()
   {
     // Given
@@ -67,5 +102,9 @@ public class CancelReservationUseCaseTest
     // When & Then
     var exception = await Assert.ThrowsAsync<ArgumentException>(() => useCase.Execute(command));
     Assert.Equal("Reservation not found", exception.Message);
+    reservationRepoMock.Verify(
+      x => x.SaveAsync(It.IsAny<Reservation>()),
+      Times.Never
+    );
   }
 }
